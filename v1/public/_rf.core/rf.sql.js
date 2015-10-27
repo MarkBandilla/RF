db7e52fd8dd78a034bdbb94e15d44e4dbfe3b6e6
Mark Daniel G. Bandilla <markbandilla.dpwm@yahoo.com>
2015-10-28 05:46:55 +0800

    add core rf.sql.js on public->_rf.core folder

---
+++ v1/public/_rf.core/rf.sql.js
@@ -0,0 +1,41 @@
+var rfSQL = {
+	db: null,
+	exec: function(params) {
+		var params = $.extend({
+			query: '',
+			values: []
+		}, params);
+		if(!this.db) return;
+		this.db.transaction(function (t) {
+	        t.executeSql(params.query, params.values, function (t, r) {
+	            return r.rows;
+	        });
+	    });
+	},
+	read: function(params) {
+		var params = $.extend({
+			query: '',
+			values: []
+		}, params);
+		if(!this.db) return;
+		this.db.readTransaction(function(t) {
+            t.executeSql(params.query, params.values, function(t, r) {
+                return r.rows;
+            });
+        });
+	},
+	connect: function(params) {
+		var params = $.extend({
+			dbname: 'db',
+			version: '1.0',
+			description: 'description'
+		}, params);
+		var odb = window.openDatabase;
+	    if(!odb) {
+	        return 'Web SQL Not Supported';
+	    } else {
+	        this.db = odb( params.dbname, params.version, params.description, 10 * 1024 * 1024 );
+	        return params.dbname + '::DB was created!';
+	    }
+	}
+};
