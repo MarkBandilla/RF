8e4ef746dc6d5abd52bfe74e246486601a7c19fd
Mark Daniel G. Bandilla <markbandilla.dpwm@yahoo.com>
2015-10-30 14:51:37 +0800

    json array sorting function

--- v1/public/_rf.core/rf.migrate.js
+++ v1/public/_rf.core/rf.migrate.js
@@ -297,3 +297,13 @@ var rfInput = [
 		]
 	}
 ]
+
+// sorting
+function sortByKey(array, key) {
+    return array.sort(function(a, b) {
+        var x = a[key]; var y = b[key];
+        return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+    });
+}
+
+people = sortByKey(people, 'name');
